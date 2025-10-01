70becb3a91fe41ee21270cf2543380b37a82a807
Guilherme Oliveira <gui.zoliveira24@gmail.com>
2025-09-30 22:34:58 -0400

    Added Jump to the player contoler
    Added
       - Player Jump that is affected by gravity and jump force
       - Changed in-game lighting

--- minigame 1/Assets/Scripts/playerControler.cs
+++ minigame 1/Assets/Scripts/playerControler.cs
@@ -17,26 +17,43 @@ public class playerControler : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float speed = 20.0f;
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    private Rigidbody playerRb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        /*--------------------------------
+        /*-----------------------------------------
         # Author: Guilherme Oliveira
         # Date: 9/29/25
-        # Desc: Moves the game object 
-        # based on the player's input
-        --------------------------------*/
+        # Desc: Moves the game object based on the 
+        # player's input
+        -----------------------------------------*/
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
